70165c7de733ec0f8a4ecc033bc5b857eab58f1e
shannon <shannonsn19@gmailcom>
2018-05-30 12:40:32 +0200

    some practise java test code from google scholarship

--- app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.android.practiceset2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // PASTE CODE YOU WANT TO TEST HERE
+        int weekday = 5;
+        int weekend = 9;
+        int optimalHours = 7 * 8;
+
+        int actualHours = weekday;
+        actualHours = actualHours + weekend * 2;
+        int solution = optimalHours - actualHours;
+        display(solution);
+
     }
+
+    /**
+     * Display methods that allow the text to appear on the screen. Don't worry if you don't know
+     * how these work yet. We'll be covering them in lesson 3.
+     */
+
+    public void display(int solution) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText("" + solution);
+    }
+
 }
